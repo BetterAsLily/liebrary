ad66e165bdfc25b902d869bf3780a7fe51bab389
Hels-lo <logphels@gmail.com>
2024-07-28 11:18:36 -0400

    #29 added the stuff and sounds too

--- project/cage.gd
+++ project/cage.gd
@@ -1,10 +1,12 @@
 extends BaseScene
 @onready var start_button = preload("res://src/ui/menu.tscn")
+@onready var canvas = $CanvasLayer
 
 func _ready():
 	super()
 	if GlobalVariables.is_first_load == true:
 		var new_button = start_button.instantiate()
-		self.add_child(new_button)
+		canvas.add_child(new_button)
 		new_button.global_position = Vector2(80,300)
+		new_button.z_index = 10
 	
