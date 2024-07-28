a39b9232419eab151c105db9f571966b02a31c81
Hels-lo <logphels@gmail.com>
2024-07-27 23:25:29 -0400

    #29 added start screen and scene

--- project/cage.gd
+++ project/cage.gd
@@ -1,1 +1,10 @@
 extends BaseScene
+@onready var start_button = preload("res://src/ui/menu.tscn")
+
+func _ready():
+	super()
+	if GlobalVariables.is_first_load == true:
+		var new_button = start_button.instantiate()
+		self.add_child(new_button)
+		new_button.global_position = Vector2(80,300)
+	
