2081e3a624421a29c68371c5f89d7b1eafe26895
BetterAsLily <lily.ashita.mo@gmail.com>
2024-07-28 17:14:48 -0400

    #27 working combat basics

--- project/src/levels/combat_1.gd
+++ project/src/levels/combat_1.gd
@@ -1,9 +1,15 @@
 extends Node2D
 
+@onready var hist = $History
+@onready var audio_stream = $AudioStreamPlayer
+@onready var enemy_sprite = $PlaceholderEnemy2
+
+var enemy_health = 3
+
 class Cauldron:
 	var step_history = []
 	var history_len = 10
-
+	
 	func add_to_queue(item):
 		if len(step_history) < history_len:
 			step_history.append(item)
@@ -22,13 +28,52 @@ class Cauldron:
 		add_to_queue(event.keycode)
 		for recipe in Cookbook.all_potions:
 			if has_recipe(recipe):
-				print("Cooked a recipe")
+				step_history = []
+				return recipe
+		return []
 
 var c = Cauldron.new()
 
 # Basic input parsing
 func _input(event):
+	var temp = []
+	var strhold = ""
+	var brewed = []
 	if event is InputEventKey and event.pressed:
-		if event.keycode == KEY_W or event.keycode == KEY_A \
-		or event.keycode == KEY_S or event.keycode == KEY_D:
-			c.handle_step(event)
+		if event.keycode == KEY_W:
+			brewed = c.handle_step(event)
+			audio_stream.stream = load("res://assets/Audio/Pot1.wav")
+			audio_stream.play()
+		elif event.keycode == KEY_A:
+			brewed = c.handle_step(event)
+			audio_stream.stream = load("res://assets/Audio/Pot2.wav")
+			audio_stream.play()
+		elif event.keycode == KEY_S:
+			brewed = c.handle_step(event)
+			audio_stream.stream = load("res://assets/Audio/Pot3.wav")
+			audio_stream.play()
+		elif event.keycode == KEY_D:
+			brewed = c.handle_step(event)
+			audio_stream.stream = load("res://assets/Audio/Pot1.wav")
+			audio_stream.play()
+		
+		if brewed == Cookbook.basic_potion:
+			print("Basic Potion!")
+			enemy_health -= 1
+			if enemy_health <= 0:
+				enemy_sprite.visible = false
+		brewed = []
+		
+		print(c.step_history)
+		for item in c.step_history:
+			if item == KEY_W:
+				strhold += "↑"
+			elif item == KEY_A:
+				strhold += "←"
+			elif item == KEY_S:
+				strhold += "↓"
+			elif item == KEY_D:
+				strhold += "→"
+		hist.text = str(strhold)
+		strhold = ""
+			
